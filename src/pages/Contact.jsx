7c11f9c838729fb5f7db16d2b7ffd119c15d7821
Anitha Minakshi <62071531+AnithaMinakshi9@users.noreply.github.com>
2020-11-12 16:00:15 +0530

    commit 2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import "./Contact.css";
 function Contact() {
-	return (
-		<>
-			<div className="main">
-				<div class="info">
-					<div class="large">Contact</div>
-					<br />
-					<div class="small">Bangalore office: +91 9999999999</div>
-					<div class="small">
-						Bangalore Address : XYZ road, ABC area
-					</div>
-					<br />
-					<div class="large">
-						<hr />
-					</div>
-					<br />
-					<div class="small">Hyderabad office: +91 8888888888</div>
-					<div class="small">
-						Hyderabad Address :MNO road, HIG area
-					</div>
-					<br />
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="main">
+        <div className="info">
+          <div className="large">Contact</div>
+          <br />
+          <div className="small">Bangalore office: +91 9999999999</div>
+          <div className="small">Bangalore Address : XYZ road, ABC area</div>
+          <br />
+          <div className="large">
+            <hr />
+          </div>
+          <br />
+          <div className="small">Hyderabad office: +91 8888888888</div>
+          <div className="small">Hyderabad Address :MNO road, HIG area</div>
+          <br />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,27 +3,27 @@ import "./Home.css";
 import { Button } from "@material-ui/core";
 
 function Home() {
-	return (
-		<>
-			<div className="top">
-				<div class="info">
-					<div class="large">Get Fit Go!</div>
-					<div class="small">
-						All progress takes place outside the comfort zone
-					</div>
-				</div>
-				<div className="buttons">
-					<Button variant="contained" color="default">
-						Book Appointment
-					</Button>
-					<br />
-					<Button variant="contained" color="default">
-						Visit us
-					</Button>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="top">
+        <div className="info">
+          <div className="large">Get Fit Go!</div>
+          <div className="small">
+            All progress takes place outside the comfort zone
+          </div>
+        </div>
+        <div className="buttons">
+          <Button variant="contained" color="default">
+            Book Appointment
+          </Button>
+          <br />
+          <Button variant="contained" color="default">
+            Visit us
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;

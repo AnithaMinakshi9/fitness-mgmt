b2e11b5f126f91ab607da32930387cf56add9511
Anitha Minakshi <62071531+AnithaMinakshi9@users.noreply.github.com>
2020-12-05 00:21:44 +0530

    Final commit

--- src/pages/User/Dashboard.js
+++ src/pages/User/Dashboard.js
@@ -18,7 +18,7 @@ import Link from "@material-ui/core/Link";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import { mainListItems, secondaryListItems } from "./listItems";
+import { mainListItems } from "./listItems";
 import Chart from "./Chart";
 import Member from "./Membership";
 
@@ -184,7 +184,6 @@ export default function Dashboard() {
         <Divider />
         <List>{mainListItems}</List>
         <Divider />
-        <List>{secondaryListItems}</List>
       </Drawer>
 
       <main className={classes.content}>
